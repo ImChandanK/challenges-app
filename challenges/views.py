4e53e7dfe3ba1bf3972fbe7582f6294c34ba166f
Chandan <ckumar.ck839@gmail.com>
2021-06-04 13:21:43 +0530

    Monthly Challenge Created

--- challenges/views.py
+++ challenges/views.py
@@ -1,9 +1,21 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here
 
 
-def index(request):
-    return HttpResponse("This Works")
+def monthly_challenge(request, month):
+
+    mappings = {
+        'january': 'The reign of Lucifer starts',
+        'february': "I'm gonna conquer the world",
+        'march': 'We reached march',
+        'april': 'We reached april',
+        'may': 'We reached may'
+    }
+
+    try:
+        return HttpResponse(mappings[month])
+    except KeyError:
+        return HttpResponseNotFound("This Month is not found")
